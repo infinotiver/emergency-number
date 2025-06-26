833eae593fc2e45c7a8063f842bb0ac5b403003e
infinotiver <88645086+infinotiver@users.noreply.github.com>
2025-06-26 21:55:25 +0530

    feat: add Flask API for retrieving emergency numbers by country code

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)  # ← This enables cross-origin requests
+
+# Serve emergency data
+import json
+
+@app.route('/api/emergency/<country_code>')
+def get_numbers(country_code):
+    country_code= country_code.upper()
+    try:
+        with open('data/data.json', 'r') as file:
+            
+            data = json.load(file)
+            print(data)
+        if country_code in data:
+            return jsonify(data[country_code])
+        else:
+            return jsonify({"error": "Country code not found"}), 404
+    except FileNotFoundError:
+        return jsonify({"error": "Data file not found"}), 500
+    except json.JSONDecodeError:
+        return jsonify({"error": "Error decoding JSON data"}), 500
+
+if __name__ == "__main__":
+    app.run(debug=True)
